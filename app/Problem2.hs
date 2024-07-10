558c2263d8b335d7214b2bdecb5461f6814f349b
nishantyaji <nishantyaji@users.noreply.github.com>
2024-07-10 17:30:21 +0000

    Solution to Problem2

---
+++ app/Problem2.hs
@@ -0,0 +1,14 @@
+-- Problem 2
+-- (*) Find the last-but-one (or second-last) element of a list.
+
+myLast :: [a] -> a
+myLast [x] = x
+myLast (x:xs) = myLast xs
+
+myButLast :: [a] -> a
+myButLast a = myLast (init a)
+
+
+-- Usage
+-- myButLast [1,2,3,4]
+-- myButLast ['a', 'b', 'c', 'd']
