1df8cfbb8668b1eb7b77a6151e2d26095751e825
nishantyaji <nishantyaji@users.noreply.github.com>
2024-07-14 04:30:05 +0000

    Problem 7: Solution copied

---
+++ Lists_Problem01_to_10/Problem7.hs
@@ -0,0 +1,18 @@
+-- Problem 7
+-- (**) Flatten a nested list structure.
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem a) = [a]
+flatten (List xs) = concatMap flatten xs
+
+-- above is same as
+-- concat $ map flatten xs
+
+-- flatten xs returns [[x]]
+-- concatMap then does [x .....]
+
+-- copied from https://www.reddit.com/r/haskellquestions/comments/3han54/how_do_you_flatten_a_nested_list_of_arbitrary/
+
+-- usage latten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
