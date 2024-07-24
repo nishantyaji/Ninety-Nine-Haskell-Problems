5a801efb539f8c809a43ac523a08a072d3197859
nishantyaji <nishantyaji@users.noreply.github.com>
2024-07-24 12:56:22 +0000

    Solution to Problem 22: ints in a Range

---
+++ Lists_Problem21_28/Problem22.hs
@@ -0,0 +1,10 @@
+-- Problem 22
+-- Create a list containing all integers within a given range.
+
+range :: Int -> Int -> [Int]
+range p q = [p .. q]
+
+-- Usage
+-- range 3 6
+-- output
+-- [3,4,5,6]
